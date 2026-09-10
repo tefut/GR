237ec510efb37ac227a5f99961db64bd43a2cfa6
tefut <wyj202321@gmail.com>
2026-09-10 17:48:34 +0800

    Update run.sh

---
+++ LLM4REC/E4REC/run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# ***********************************************************************
+# Copyright: (c) Huawei Technologies Co., Ltd. 2021. All rights reserved.
+# 训练入口
+# version: 1.0.0
+# change log:
+# ***********************************************************************
+
+echo "running run.sh"
+train_workdir=$(cd $(dirname $0); pwd)
+echo ${train_workdir}
+
+python ${train_workdir}/utils/data_preprocess.py \
+      --user_file_path=${user_file_path}   \
+      --item_file_path=${item_file_path}   \
+      --data_path=${data_path}
+
+python ${train_workdir}/finetune.py \
+    --base_model=${base_model} \
+    --data_path=${data_path} \
+    --cache_dir=${cache_dir} \
+    --output_dir=${output_dir} \
+    --item_embedding_file=${item_embedding_file} \
+    --train_config_file=${train_config_file} \
+    --distribute_type=${distribute_type}
