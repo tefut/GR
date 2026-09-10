8b90cb805d4c5a640a3531c05f9809be0f7dcb16
tefut <wyj202321@gmail.com>
2026-09-10 19:06:25 +0800

    Update constant_game.py

---
+++ LLM4REC/KAR/constant_game.py
@@ -0,0 +1,16 @@
+# game
+GAME_ID = 'app_id'
+GAME_NAME = 'app_cn_name'
+GAME_PACKAGE_NAME = 'package_name'
+GAME_PACKAGE_NAME_SEP = '.'
+GAME_TAG = 'tags'
+GAME_APP_DESC = 'app_desc'
+GAME_DEVELOPER_CN_NAME = 'developer_cn_name'
+GAME_APP_SECOND_TYPE = 'app_second_type'
+GAME_APP_THIRD_TYPE = 'app_third_type'
+GAME_SEARCH_TAG = 'search_tag1'
+GAME_SPLIT_PACKAGE_NAME = 'split_package_name'
+
+FULL_ITEM_GAME_COLUMNS = [GAME_ID, GAME_NAME, GAME_PACKAGE_NAME, GAME_TAG, GAME_APP_DESC, GAME_DEVELOPER_CN_NAME,
+                          GAME_APP_SECOND_TYPE,
+                          GAME_APP_THIRD_TYPE, GAME_SEARCH_TAG, GAME_SPLIT_PACKAGE_NAME]
